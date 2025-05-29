73cb1d3946b3399339b28cabb0c37e2f82079b4d
Honghao329 <honghao.liu@ucconnectd.ie>
2025-05-29 16:13:04 +0800

    i

---
+++ src/main/java/team/group/myforbidden/model/Treasure.java
@@ -0,0 +1,9 @@
+package team.group.myforbidden.model;
+
+public class Treasure {
+    private TreasureType treasureType;
+
+    public Treasure(TreasureType treasureType) {
+        this.treasureType = treasureType;
+    }
+}
